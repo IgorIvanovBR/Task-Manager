8868d7dfea15c8a09f31b32765e2a0973f151a65
Igor <igorivanov96work@gmail.com>
2020-08-03 00:20:16 +0300

    Added local storage logic and styles
    TODO:
    fix problem with local storage writing
    add auto load from local storage
    put "add task form" in modal window

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, {useState} from 'react';
 import './styles/style.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import envelope from './media/envelope.png'
+import plus from './media/plus.png'
+import remove from './media/remove.png'
 
 
 function TaskManager() {
@@ -8,6 +11,12 @@ function TaskManager() {
     const [newDesk, setNewDesk] = useState('')
     const [tasks, setTasks] = useState([])
     const [count, setCount] = useState(1);
+
+    function  addTask() {
+      document.getElementById('form-wrapper').classList.toggle('visible')
+    }
+
+
 
     function handleNewTask(e) {
       e.preventDefault()
@@ -20,49 +29,75 @@ function TaskManager() {
         setNewDesk(e.target.value)
     }
 
-    function addNewTask(e){
-        e.preventDefault()
-        if (newTask  === '' )return
-        else if (newDesk === '')return
-        setCount(count + 1)
-        setTasks([...tasks, {id: Date.now(), number: count, task: newTask, desk: newDesk}])
-        console.log(newTask, newDesk)
+    function addNewTask(){
+
+        if (newTask  === '' ) {
+            return
+        } else if (newDesk === '') {
+            return
+        }
+        if (localStorage.getItem('tasks') == null) {
+            setCount(count + 1)
+            setTasks([...tasks, {id: Date.now(), number: count, task: newTask, desk: newDesk}])
+            localStorage.setItem('tasks', JSON.stringify({id: Date.now(), number: count, task: newTask, desk: newDesk}))
+            console.log({id: Date.now(), number: count, task: newTask, desk: newDesk})
+            document.getElementById('form-wrapper').classList.toggle('visible')
+        }
+        else {
+            setCount(count + 1)
+            localStorage.setItem('tasks', JSON.stringify(tasks))
+            setTasks([...tasks, {id: Date.now(), number: count, task: newTask, desk: newDesk}])
+            document.getElementById('form-wrapper').classList.toggle('visible')
+        }
     }
 
     function removeTask(id){
         setCount(count - 1)
        setTasks(tasks.filter((task) => task.id !== id))
+        localStorage.setItem('tasks', JSON.stringify(tasks))
+
     }
 
     return (
-            <div className="wrapper">
-                <div className="form-wrap">
+            <div className="container">
+                <div className="row">
+                <div className="col-md-2"> </div>
+                <div className="col-md-8 main-wrapper ">
+                <div className="form-wrap visible" id="form-wrapper">
                     <form onSubmit={addNewTask}>
-                        <input type="text" placeholder="Task Name" onChange={handleNewTask} />
-                        <input type="text" placeholder="Task Description" onChange={handleNewDesk}/>
+                        <input type="text" placeholder="Task Name" onChange={handleNewTask} /><br/>
+                        <input type="text" placeholder="Task Description" onChange={handleNewDesk}/><br/>
                         <input type="button" value="Add Task" className="btn btn-success" onClick={addNewTask}/>
-                        <input type="button" value="X" className="btn btn-danger"/>
+                        <input type="button" value="X" onClick={addTask}  className="btn btn-danger"/>
                     </form>
                 </div>
-                <h1>Easy Tasks</h1>
+                <h1 className="heading">Easy Tasks</h1>
+                <input type="button" onClick={addTask} value="Add Task" className="addTaskButton"/>
                 <table className="tasks-table">
                     <tbody>
                     <tr>
-                        <td>Number</td>
+                        <td>#</td>
+                        <td><img src={envelope} width="24" height="24" alt="letter"/></td>
                         <td>Task Name</td>
                         <td>Task Description</td>
                         <td>X</td>
                     </tr>
-                    {tasks.map((task) => (
+                    {tasks.map((task) =>(
                         <tr key={task.id}>
                          <td>{task.number}</td>
+                            <td><img src={envelope} width="24" height="24" alt="letter"/></td>
                             <td>{task.task}</td>
                             <td>{task.desk}</td>
-                            <td><input type="button" value='X' className="btn btn-danger" onClick={()=> removeTask(task.id)}/></td>
+                            <td><input type="button"  value='X' className="btn btn-danger" onClick={()=> removeTask(task.id)}/></td>
                         </tr>
-                    ))}
+                    ))
+                    }
+
                     </tbody>
                 </table>
+                </div>
+            <div className="col-md-2"> </div>
+                </div>
             </div>
         );
 }
